41e197f976f34e3cfe5bea8f73895b96fb34511e
Oksana Brunko <101710955+SXuha@users.noreply.github.com>
2022-07-10 13:42:26 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,67 +1,75 @@
-// Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. 
-// Подивися демо відео роботи галереї.
-
-// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
-
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-// Реалізація делегування на div.gallery і отримання url великого зображення.
-// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. 
-// Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. 
-// Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
-
-// Розмітка елемента галереї
-// Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>, і вказуватися в href посилання. 
-// Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
-
-// <div class="gallery__item">
-  // <a class="gallery__link" href="large-image.jpg">
-     //<img
-       //class="gallery__image"
-       //src="small-image.jpg"
-      //data-source="large-image.jpg"
-      //alt="Image description"
-    ///>
-  //</a>
-//</div>
-//Зверни увагу на те, що зображення обгорнуте посиланням, 
-//отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку. 
-//Заборони цю поведінку за замовчуванням.
-
-
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const gallery = document.querySelector('div.gallery');
-const imagesGallery = galleryItems.map((el) => {
-    return `<div class="gallery__item">
-    <a class="gallery__link" href="${el.original}">
-      <img
-        class="gallery__image"
-        src="${el.preview}"
-        data-source="${el.original}"
-        alt="${el.description}"
-      />
-    </a>
-  </div>`
-}).join(""); 
-gallery.insertAdjacentHTML("afterbegin", imagesGallery);
-console.log(gallery);
 
+console.log(galleryItems);
 
+// 1.Створення і рендер розмітки на підставі масиву даних galleryItems
+// і наданого шаблону елемента галереї.
 
+const galleryBlockEl = document.querySelector(".gallery");
 
-gallery.addEventListener("click", onGalleryClick ); 
+const makeGalleryMarkup = ({ preview, original, description }) => {
+  return `
+<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+};
 
-function onGalleryClick (event) {
+const makeGallery = galleryItems.map(makeGalleryMarkup).join("");
+
+galleryBlockEl.insertAdjacentHTML("afterbegin", makeGallery);
+
+// 2. Реалізація делегування на div.gallery і отримання url великого зображення.
+
+galleryBlockEl.addEventListener("click", selectImg);
+
+function selectImg(event) {
   event.preventDefault();
 
-  if (event.target.nodeName !== 'IMG') return;
-  const instance = basicLightbox.create(`
-      <img src="${event.target.dataset.source}" width="800" height="600">
-  `)
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
 
-  instance.show()
-}
+  onImgClick(event.target.dataset.source);
+};
 
+// 3. Підключення скрипту і стилів бібліотеки модального вікна basicLightbox.
+// Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані(.min) файли бібліотеки.
+/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css"/> */
+// <script src="https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js"></script>
 
+// 4. Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+// 5. Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+// 6. Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. Бібліотекаи basicLightbox містить метод для програмного закриття модального вікна.
+
+let instance;
+
+function onImgClick(source) {
+  instance = basicLightbox.create(`<img src="${source}">`, {
+    onShow: () => {
+      window.addEventListener("keydown", onEskPress);
+    },
+    onClose: () => {
+      window.removeEventListener("keydown", onEskPress);
+    },
+  });
+
+  instance.show();
+};
+
+function onEskPress(event) {
+  const ESC_KEY = "Escape";
+
+  if (event.code === ESC_KEY) {
+    instance.close();
+  }
+
+  console.log(event);
+};
